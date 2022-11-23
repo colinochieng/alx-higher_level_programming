1d6f581fc38ebb786c94076fd365047cfa3d4c0d
codewithLevis <levisomondi368@gmail.com>
2022-11-23 16:11:33 +0300

    Initial commit for Colins the beginner

---
+++ colin.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <window>
+
+
+int main (){
+  char fName[], sName[];
+    printf("Welcome to Xi shop");
+    printf(" Enter your first name\n");
+    scanf("%s" , fName);
+    printf(" Enter your second name\n");
+    scanf("%s" , sName);
+    printf(" Hello, %S %S \n Feel free to shop in ")
+
+}
