d52a3491f4654e915e3bffe0781706c0c8e3922d
colinochieng <colinokumu@gmail.com>
2023-03-07 11:54:50 -0800

    Practice Perfects

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(0, 99):
-    print('{:d}'.format(i), end = " = ")
-    print('{}'.format(hex(i)))
+for i in range(0, 100):
+    print("{} = 0x{:x}".format(i, i))
