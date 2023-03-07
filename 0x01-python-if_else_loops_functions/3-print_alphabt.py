0bb22a6ddbdfa3ff857dda5820cacd3577601b95
colinochieng <colinokumu@gmail.com>
2023-03-07 08:08:36 -0800

    Practice Perfects

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if i == ord('q') or i == ord('e'):
+for i in range(ord('a'), ord('{')):
+    if chr(i) == 'e' or chr(i) == 'q':
         continue
-    print(chr(i) , end="")
-
-
+    print(f'{i:c}', end='')
