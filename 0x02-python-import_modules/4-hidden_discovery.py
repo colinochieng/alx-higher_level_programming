ba455389328a953628ff4106dafd4630a25620c7
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-10 01:17:32 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 if __name__ == "__main__":
-    """Print all names defined by defined
-    by the compiled module hidden_4.pyc"""
+   
     import hidden_4
 
     names = dir(hidden_4)
     for name in names:
-        if name[:2] != "__":
+        if name[:2] == "__":
+           continue
+        else:
             print(name)
