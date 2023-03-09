d9e44d476207edfa6d719f27bc60a6fc62969ebc
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-09 23:16:28 +0300

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import calculator_1 as calc
+if __name__ == "__main__":
+    import calculator_1 as calc
 
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-tot = calc.add(a, b)
-pro = calc.mul(a, b)
-quotient = calc.div(a, b)
-diff = calc.sub(a, b)
+    tot = calc.add(a, b)
+    pro = calc.mul(a, b)
+    quotient = calc.div(a, b)
+    diff = calc.sub(a, b)
 
-print('{0} + {1} = {2}\n{0} -\
- {1} = {3}'.format(a, b, tot, diff))
-print('{0} * {1} = {2}\n{0} /\
- {1} = {3}'.format(a, b, pro, quotient))
+    print('{0} + {1} = {2}\n{0} -\
+    {1} = {3}'.format(a, b, tot, diff))
+    print('{0} * {1} = {2}\n{0} /\
+    {1} = {3}'.format(a, b, pro, quotient))
