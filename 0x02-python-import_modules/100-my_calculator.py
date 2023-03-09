8a1a3490a3c46e63fe4db21143cd900aa14207e9
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-10 01:39:57 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,21 +4,22 @@ from calculator_1 import calc
 
 
 if __name__ == "__main__":
-    
-    if (len(argv) - 1) != 3:
+
+    count = len(argv) - 1
+    if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    sign = argv[2]
+    op = argv[2]
     a = int(argv[1])
     b = int(argv[3])
-    if sign == "+":
-        print("{} + {} = {}".format(a, b, calc.add(a, b)))
-    elif sign == "-":
-        print("{} - {} = {}".format(a, b, calc.sub(a, b)))
-    elif sign == "*":
-        print("{} * {} = {}".format(a, b, calc.mul(a, b)))
-    elif sign == "/":
-        print("{} / {} = {}".format(a, b, calc.div(a, b)))
+    if op == "+":
+        print("{} + {} = {}".format(a, b, (calc.add(a, b))))
+    elif op == "-":
+        print("{} - {} = {}".format(a, b, (calc.sub(a, b))))
+    elif op == "*":
+        print("{} * {} = {}".format(a, b, (calc.mul(a, b))))
+    elif op == "/":
+        print("{} / {} = {}".format(a, b, (calc.div(a, b))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
