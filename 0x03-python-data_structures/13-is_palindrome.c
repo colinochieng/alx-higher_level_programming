a6a71b4c8d62889b2c585818d55ddeed06914dac
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-10 19:13:23 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,43 +2,39 @@
 
 int is_palindrome(listint_t **head)
 {
-        listint_t *start, *end, *ptr;
-        int i, j , k;
+	listint_t *start, *end, *ptr;
+	int i, j , k;
 
-        if (*head == NULL)
-                return (1);
+	if (*head == NULL)
+		return (1);
 
-        ptr = *head;
-        i = 0;
-        while (ptr != NULL)
-        {
-                i++;
-                ptr = ptr->next;
-        }
+	ptr = *head;
+	i = 0;
+	while (ptr != NULL)
+	{
+		i++;
+		ptr = ptr->next;
+	}
 
-        if (i % 2 != 0)
-                return (0);
-        else
-        {
-                start = *head;
-                j = i / 2 ;
+	start = *head;
+	j = i / 2 ;
 
-                while (start != NULL && j >= 0)
-                {
-                        end = *head;
-                        k = 0;
-                        while (end != NULL && k < i - 1)
-                        {
-                                k++;
-                                end = end->next;
-                        }
-                        if (start->n != end->n)
-                                return (0);
-                        start = start->next;
-                        j--;
-                        i--;
-                }
-        }
+	while (start != NULL && j >= 0)
+	{
+		end = *head;
+		k = 0;
+		while (end != NULL && k < i - 1)
+		{
+			k++;
+			end = end->next;
+		}
+		if (start->n != end->n)
+			return (0);
 
-        return (1);
+		start = start->next;
+		j--;
+		i--;
+	}
+
+	return (1);
 }
