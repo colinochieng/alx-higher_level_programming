3705a0fe9bdcbe4bf1795db2513843884e7a7777
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-10 13:01:08 +0300

    Create 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    new = []
+    for i in my_list:
+        if i % 2 == 0:
+            i = True
+        else:
+            i = False
+        new.append(i)
+    return (new)
