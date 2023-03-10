b74c49d0cd37f0c0a6227097c3eaf21a89571793
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-10 11:49:28 +0300

    Create 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    value = len(my_list) - 1
+    if idx < 0 or value < idx:
+        return (my_list)
+    else:  
+        new = []
+        i = 0
+        while i < value:
+            new.append(my_list[i])
+            i += 1
+        new[idx] = element
+        return (new)
