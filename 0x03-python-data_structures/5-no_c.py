89bbe0e8acb0d0c8cf614b544813b64c90dcb97b
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-10 12:03:59 +0300

    Create 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    stri = ''
+    for j in my_string:
+       if j == 'c' or j == 'C':
+          continue
+       else:
+          stri += j
+    return (stri)
