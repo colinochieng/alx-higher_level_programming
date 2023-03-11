a21b07eb5a98b60b50c18cf8bb0577c904ebbdcb
colinochieng <colinokumu@gmail.com>
2023-03-11 13:54:43 -0800

    Practice Perfects

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,10 +3,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-
-    j = my_list[0]
-    for i in my_list:
-        if i > j:
-            j = i;
-        
-    return (j)
+    else:
+        j = my_list[0]
+        for i in my_list:
+            if i > j:
+                j = i;
+        return (j)
