ac8e983379d769bb3f9061f8dbef26369ffdf616
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 03:01:34 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def double(a):
-        return a * 2
-    
+    return a * 2
+
 
 def multiply_by_2(a_dictionary):
-    double = lambda a: a * 2
     new_dic = {}
     for key in a_dictionary:
         new_dic[key] = double(a_dictionary[key])
