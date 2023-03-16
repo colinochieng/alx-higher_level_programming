7a7192921744b2c6930871df2ec196b64c3485cf
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 04:34:37 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -8,7 +8,7 @@ def weight_average(my_list=[]):
 
     if len(my_list) == 0:
         return (0)
-    for i in  my_list:
+    for i in my_list:
         j, k = i
         ls1.append(j)
         ls2.append(k)
