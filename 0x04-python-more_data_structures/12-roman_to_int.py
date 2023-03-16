38245043387df6cb74ba69b4f0a001ea7966797d
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 04:55:40 +0300

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if (roman_string is None) or (type(roman_string) is not str):
+        return 0
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_val = 0
+
+    for val in roman_string[::-1]:
+        curr_val = roman_dict[val]
+        if curr_val < prev_val:
+            result -= curr_val
+        else:
+            result += curr_val
+        prev_val = curr_val
+
+    return result
