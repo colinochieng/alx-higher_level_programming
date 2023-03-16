1bbeaa890489f458d5539ea96865d1232fd1156c
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 04:43:46 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,13 +3,4 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-
-    ls = [a_dictionary.get(key) for key in a_dictionary]
-    maxim = lambda a, b: a if a > b else b
-    i = ls[0]
-    for j in ls:
-        k = maxim(i, j)
-        i = j
-    for ke, v in a_dictionary.items():
-        if v == k:
-            return ke
+    return max(a_dictionary, key=a_dictionary.get)
