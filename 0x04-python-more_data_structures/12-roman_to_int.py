5e70e721f3280136fe240810aa91771ffa7317bb
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 05:00:01 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,12 @@
 def roman_to_int(roman_string):
     if (roman_string is None) or (type(roman_string) is not str):
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10,\
-                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     prev_val = 0
 
     for val in roman_string[::-1]:
-        curr_val = roman_dict[val]
+        curr_val = rom_dic[val]
         if curr_val < prev_val:
             result -= curr_val
         else:
