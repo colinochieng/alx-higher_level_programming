a427537b745998ca21117189c21e38e40d1a3dde
colinochieng <colinokumu@gmail.com>
2023-03-28 15:18:04 -0700

    OOP

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Private instance attribute"""
+
+
+class Square:
+    def __init__(self, size):
+        """Defining new square function
+        Args:
+        :param size: square size
+        """
+        self.__size = size
