cc96ee5d2bd9fb3d11dbffb7f2eab5e7dd4c3aa2
colinochieng <colinokumu@gmail.com>
2023-03-29 15:35:23 -0700

    OOP

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+"""Private instance attribute: size"""
+
+
+class Square:
+    """Define Square and check for Error"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Value setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve value"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets Value"""
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print("")
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
+
+    def __str__(self):
+        res = ""
+        if self.__size == 0:
+            res += "\n"
+        else:
+            for i in range(self.__position[1]):
+                res += "\n"
+            for i in range(self.__size):
+                res += " " * self.__position[0] + "#" * self.__size + "\n"
+        return res[:-1]
