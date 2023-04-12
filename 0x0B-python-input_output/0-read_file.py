6dbca27cf0096cecdb8e73cf68eb0aae9e33ebe1
colinochieng <colinokumu@gmail.com>
2023-04-12 16:44:38 -0700

    Input and Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     (UTF8) and prints it to stdout
     """
     with open(filename, encoding='utf-8') as fl:
-        print(fl.read(),end="")
+        print(fl.read(), end="")
