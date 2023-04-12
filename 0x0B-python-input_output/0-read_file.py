0d49e00c29aa2feed10829cd3c57e923bb035a26
colinochieng <colinokumu@gmail.com>
2023-04-12 16:43:10 -0700

    Input and Output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Testing Python Input and Output"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file
+    (UTF8) and prints it to stdout
+    """
+    with open(filename, encoding='utf-8') as fl:
+        print(fl.read(),end="")
