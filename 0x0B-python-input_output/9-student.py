59936af882269b5f1126e5fa3e7b6b4986ca5494
colinochieng <colinokumu@gmail.com>
2023-04-12 19:18:23 -0700

    JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,7 +4,6 @@
 
 class Student:
     """class Student that defines a student"""
-    
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
