66256b02d1e4d96b1a5ff51edc629a7c455a61cb
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-04-18 02:46:28 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,16 +7,12 @@ class Square(Rectangle):
     """Derived class of rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initializes from Rectangle"""
         super().__init__(size, size, x=x, y=y, id=id)
 
     def __str__(self):
-        """ str special method """
-        str_square = "[Square] "
-        str_id = "({}) ".format(self.id)
-        str_xy = "{}/{} - ".format(self.x, self.y)
-        str_wh = "{}/{}".format(self.width, self.height)
-
-        return str_square + str_id + str_xy + str_wh
+        """Square User String representation"""
+        return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
 
     @property
     def size(self):
