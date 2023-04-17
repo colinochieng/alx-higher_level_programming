e22a4aa069d252812932d91e87556f148585efef
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-04-18 02:00:09 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,8 +35,9 @@ class Base:
         file = cls.__name__ + '.json'
         with open(file, mode='w', encoding='utf-8') as fi:
             """"""
-            fi.write(cls.to_json_string\
-                     ([obj.to_dictionary() for obj in list_objs]))
+            fi.write(cls.to_json_string (
+                [obj.to_dictionary() for obj in list_objs]
+            ))
 
     @staticmethod
     def from_json_string(json_string):
@@ -86,7 +87,7 @@ class Base:
         with open(filename, "w") as file:
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
-            writer.writerows(rows)       
+            writer.writerows(rows)
 
     @classmethod
     def load_from_file_csv(cls):
