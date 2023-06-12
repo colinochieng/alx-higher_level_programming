022f307bbdb5f3d4b37f55aa23ec664f04225492
colinochieng <colinokumu89@gmail.com>
2023-06-12 22:17:53 +0000

    js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 
-const joined = process.argv.slice(2, 4).join(' is ');
-
 console.log(`${process.argv[2]} is ${process.argv[3]}`);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const num = parseFloat(process.argv[2]);
 
 if (!isNaN(num)) {
-	console.log('My number: ' + process.argv[2]);
+  console.log('My number: ' + process.argv[2]);
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }
