c0506d0ef91a9fd310876a64db8d6cb87abbb83a
colinochieng <colinokumu89@gmail.com>
2023-06-12 22:11:28 +0000

    js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,9 @@
 const argv = process.argv.length - 2;
 
 if (argv === 0) {
-console.log('No argument');
+  console.log('No argument');
 } else if (argv === 1){
   console.log('Argument found');
-}
-else {
- console.log('Arguments found');
+} else {
+  console.log('Arguments found');
 }
