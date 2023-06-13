f879952c6ff65ed72e2e49226bb1b57a0645697d
colinochieng <colinokumu89@gmail.com>
2023-06-13 13:02:19 +0000

    js objects

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    
+    while (i < this.height) {
+      let j = 0;
+      let raw = '';
+
+      while (j < this.width) {
+        raw += 'X';
+        j++;
+      }
+      console.log(raw);
+      i++;
+    }
+  }
+};
