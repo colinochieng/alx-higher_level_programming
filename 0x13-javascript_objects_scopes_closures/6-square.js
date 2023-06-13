99fd325421f909ee9f2f4f73daf9f3063e3003ff
colinochieng <colinokumu89@gmail.com>
2023-06-13 19:25:26 +0000

    js objects

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,9 +3,8 @@ module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
-      }
-    for (let i = 0; i < this.height; i++)
-    {
+    }
+    for (let i = 0; i < this.height; i++) {
       let str = '';
       for (let j = 0; j < this.width; j++) {
         str += c;
