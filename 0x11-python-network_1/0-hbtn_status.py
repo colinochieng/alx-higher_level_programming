8358b9a68518e8f822fcffaed05940c0bda942fc
colinochieng <colinokumu89@gmail.com>
2023-06-30 23:46:52 +0300

    python networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,11 @@ A module that fetches https://alx-intranet.hbtn.io/status
 use the package urllib
 Opens the link accass th type of data and decodes it
 """
-from  urllib.request import urlopen
+from  urllib import request
 
 
 if __name__ == "__main__":
-    with urlopen("https://alx-intranet.hbtn.io/status") as access:
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as access:
         data = access.read()
         print("Body response:")
         print("\t- type:", type(data))
