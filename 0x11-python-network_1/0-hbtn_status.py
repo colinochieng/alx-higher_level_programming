bced4e65dc05ba02e731dbd07341ba8bafeaca1a
colinochieng <colinokumu89@gmail.com>
2023-06-30 23:37:59 +0300

    python networks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,10 @@ Opens the link accass th type of data and decodes it
 import urllib.request
 
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as access:
-    data = access.read()
-    print("Body response:")
-    print("\t- type: ", type(data))
-    print('\t- content: ', data)
-    print("\t- utf8 content: ", data.decode('utf-8'))
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as access:
+        data = access.read()
+        print("Body response:")
+        print("\t- type: ", type(data))
+        print('\t- content: ', data)
+        print("\t- utf8 content: ", data.decode('utf-8'))
