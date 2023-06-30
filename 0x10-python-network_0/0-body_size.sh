31e0ea3b258de8720e294e54575ce4489c880536
colinochieng <colinokumu@gmail.com>
2023-06-30 01:59:08 -0700

    http

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl "$1" | grep -iE "^Content-Length: " | awk -F ': ' '{print $2}'
+curl "$1" | wc -c
