e08f9974e609f0d2907fac08faf9dfebe0c1f540
colinochieng <colinokumu89@gmail.com>
2023-07-04 23:59:25 +0300

    check

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,14 +6,21 @@ and displays the body of the response (decoded in utf-8)
 """
 
 from urllib.request import urlopen, Request
+from urllib.error import URLError, HTTPError
+from urllib.parse import urlencode
 from sys import argv
-import json
 
-email = {"email": argv[2]}
-data = json.dumps(email).encode('utf-8')
-query = Request(argv[1], data=data, method="POST", headers={'Content-Type': 'application/json'})
-
-with urlopen(query) as access:
-    response_data = access.read().decode('utf-8')
-    response_json = json.loads(response_data)
-    print(response_json['Your email is'])
+if __name__ == "__main__":
+    email = {"email": argv[2]}
+    data = urlencode(email).encode('utf-8')
+    query = Request(argv[1], data=data, method="POST")
+    try:
+        with urlopen(query) as access:
+            response_data = access.read().decode('utf-8')
+            print(response_data)
+    except ValueError:
+        print("Invalid URL or email")
+    except URLError as e:
+        print("Error opening URL: {}".format(e.reason))
+    except HTTPError as e:
+        print("Error sending request: {} {}".format(e.code, e.reason))
