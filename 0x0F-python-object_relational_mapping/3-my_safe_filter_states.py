3e22cdaad9a8e9b11957414953fad85eb2b2aff8
colinochieng <colinokumu89@gmail.com>
2023-07-15 13:19:31 +0300

    MySQLdb

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database hbtn_0e_0_usa
+Usage: ./2-my_filter_states.py [username] [password] [database] [state name]
+"""
+import sys
+import MySQLdb
+
+
+def filter_states(username, password, database, state):
+    conn = MySQLdb.Connect(user=username, passwd=password, db=database)
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'
+            ORDER BY states.id ASC""".format(state))
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4].split(';')[0].strip("’")
+    filter_states(username, password, database, state)
