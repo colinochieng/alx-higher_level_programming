758be212cf287417420e9d437cc05cf4376f0db9
colinochieng <colinokumu89@gmail.com>
2023-07-15 12:43:10 +0300

    MySQLdb

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,9 +10,7 @@ import MySQLdb
 def filter_states(username, password, database, state):
     conn = MySQLdb.Connect(user=username, passwd=password, db=database)
     cursor = conn.cursor()
-    cursor.execute("""SELECT * FROM states 
-                   WHERE name LIKE BINARY '{}' 
-                   ORDER BY states.id ASC""".format(state).strip("'"))
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state))
     result = cursor.fetchall()
     for row in result:
         print(row)
@@ -20,29 +18,9 @@ def filter_states(username, password, database, state):
     conn.close()
 
 
-# if __name__ == "__main__":
-#     username = sys.argv[1]
-#     password = sys.argv[2]
-#     database = sys.argv[3]
-#     state = sys.argv[4]
-#     filter_states(username, password, database, state)
-
-if __name__ == '__main__':
-    if len(sys.argv) >= 5:
-        db_connection = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-        )
-        cursor = db_connection.cursor()
-        state_name = sys.argv[4]
-        cursor.execute(
-            'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
-            'CAST("{}" AS BINARY) ORDER BY id ASC;'.format(state_name)
-        )
-        results = cursor.fetchall()
-        for result in results:
-            print(result)
-        db_connection.close()
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+    filter_states(username, password, database, state)
