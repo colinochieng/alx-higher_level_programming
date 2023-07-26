30b4f69c2bca27c67157e79a3e6138ac7e5fee5d
colinochieng <colinokumu89@gmail.com>
2023-07-26 06:16:43 -0700

    API

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,21 @@
-#!/usr/bin/log
-const request = require('request')
+#!/usr/bin/node
+const request = require('request');
+const uri = 'https://swapi-api.alx-tools.com/api/people/18/';
+let count = 0;
 
 request.get(process.argv[2], (err, data, body) => {
   if (err) {
     console.error(err);
   }
-  let json_data = JSON.parse(body);
-  for ()
+  const jsonData = JSON.parse(body);
+  const array = [];
+  for (let i = 0; jsonData.results[i]; i++) {
+    array.push(jsonData.results[i].characters);
+  }
+  for (let idx = 0; array[idx]; idx++) {
+    if (array[idx].includes(uri)) {
+      count++;
+    }
+  }
+  console.log(count);
 });
